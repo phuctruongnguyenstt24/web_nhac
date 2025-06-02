900c5efae59373bb46eae682ee7abf1c2acbf9e4
Phuczalo <102162686+phuctruongnguyenstt24@users.noreply.github.com>
2025-06-02 22:46:36 +0700

    Update song.js

--- song.js
+++ song.js
@@ -1,21 +1,21 @@
 let songs=[
 {
     name:'sadness and sorrow',
-    path:'MUSIC_one/Naruto Soundtrack- Sadness and Sorrow (FULL VERSION).mp3' ,
+    path:'Music/Naruto Soundtrack- Sadness and Sorrow (FULL VERSION).mp3' ,
     artist:'Yasuharu Takanashi và Toshio Masuda',
     image :'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVIUqSE5-e36VRxz8eXLxiyC-k8dHUUfDKeA&s'
     
 },
 {
     name:'HOA BANG LANG RAP',
-    path:'MUSIC_one/Hoa Bằng Lăng  Rap Version  Spy Official ( Tam Kê Mix ).mp3' ,
+    path:'Music/Hoa Bằng Lăng  Rap Version  Spy Official ( Tam Kê Mix ).mp3' ,
     artist:'RApper',
     image :'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTSAo2F_6fi-ZeHDMUHUkSlD5DpEsja1970w&s'
     
 },
 {
     name:'Mot buoc yeu van dam',
-    path:'MUSIC_one/MỘT BƯỚC YÊU VẠN DẶM ĐAU  Mr. Siro - Day 5 Fanconcert HCM.mp3' ,
+    path:'Music/MỘT BƯỚC YÊU VẠN DẶM ĐAU  Mr. Siro - Day 5 Fanconcert HCM.mp3' ,
     artist:'Mr.Siro',
     image :' https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfal0onZfxn_bOBTxQcnaL8xWB20Bxrc23Hg&s'
     
@@ -23,7 +23,7 @@ let songs=[
 
 {
     name:'Lưu Luyến Sau Chia Tay',
-    path:'MUSIC_one/Lưu Luyến Sau Chia Tay - Try92 ft. Kai06 ｜ Official Lyrics Video.mp3' ,
+    path:'Music/Lưu Luyến Sau Chia Tay - Try92 ft. Kai06 ｜ Official Lyrics Video.mp3' ,
     artist:'Chí Thiện',
     image :' https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAqxGBADdH9bKUtvlLSlkb1QOXOLjF078J_g&s'
     
